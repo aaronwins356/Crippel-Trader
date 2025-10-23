879b121938480347ff9f51efed139f7fde8cb3e9
Aaron Wins <your-email@example.com>
2025-10-23 13:30:28 -0700

    chore: clear repository contents

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-  testMatch: ['**/tests/**/*.js'],
-  testTimeout: 20000
-};
--- pybackend/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Python trading backend for Crippel Trader."""
-
-from .server import app
-
-__all__ = ["app"]
